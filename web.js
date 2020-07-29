98c9f6876698a9ae928acf8670c6002ca3a252bc
Scott Doxey <neogeek0101@gmail.com>
2020-07-29 00:53:29 -0400

    Updated packages.
    Formatted files.

--- web.js
+++ web.js
@@ -4,43 +4,45 @@ const fs = require('fs');
 
 const restify = require('restify');
 
-const random = (array) => array[Math.floor(Math.random() * array.length)];
+const random = array => array[Math.floor(Math.random() * array.length)];
 
 const JSON_FILE_REGEX = /\.json$/;
 
-const datasets = Object.assign({}, ...fs.readdirSync('data/')
-    .filter((file) => file.match(JSON_FILE_REGEX))
-    .map((file) => {
-
-        return {
-            [file.replace(JSON_FILE_REGEX, '')]:
-                JSON.parse(fs.readFileSync(`data/${file}`, 'utf8'))};
-
-    }));
+const datasets = Object.assign(
+    {},
+    ...fs
+        .readdirSync('data/')
+        .filter(file => file.match(JSON_FILE_REGEX))
+        .map(file => {
+            return {
+                [file.replace(JSON_FILE_REGEX, '')]: JSON.parse(
+                    fs.readFileSync(`data/${file}`, 'utf8')
+                )
+            };
+        })
+);
 
 const server = restify.createServer();
 
-server.get('/:command', restify.bodyParser(), (req, res) => {
+server.use(restify.plugins.bodyParser());
 
+server.get('/:command', (req, res) => {
     if (datasets[req.params.command]) {
-
         return res.send({
-            'attachments': [
+            attachments: [
                 {
-                    'fallback': req.params.command,
-                    'image_url': random(datasets[req.params.command])
+                    fallback: req.params.command,
+                    image_url: random(datasets[req.params.command])
                 }
             ],
-            'response_type': 'in_channel'
+            response_type: 'in_channel'
         });
-
     }
 
     return res.send({
-        'response_type': 'ephemeral',
-        'text': `Whoops! No slash command was found for *${req.params.command}*.`
+        response_type: 'ephemeral',
+        text: `Whoops! No slash command was found for *${req.params.command}*.`
     });
-
 });
 
 server.listen(process.env.PORT);
